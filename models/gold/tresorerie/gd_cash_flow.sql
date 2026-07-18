b00c970dc32a7ac7d442ea6a466fdb4fe0c5e4a6
Abdoul <azbkbah@gmail.com>
2026-07-18 16:10:37 +0200

    refactor: update SQL logic for cash flow model and improve compatibility with BigQuery

--- models/gold/tresorerie/gd_cash_flow.sql
+++ models/gold/tresorerie/gd_cash_flow.sql
@@ -10,19 +10,19 @@ SELECT
     annee_mois, 
     annee, 
     mois, 
-    
+     
     CASE 
         WHEN mois BETWEEN 1 AND 3 THEN 1
         WHEN mois BETWEEN 4 AND 6 THEN 2
         WHEN mois BETWEEN 7 AND 9 THEN 3
         WHEN mois BETWEEN 10 AND 12 THEN 4
     END                          AS trimestre, 
-    
+     
     total_debit                  AS encaissements, 
     total_credit                 AS decaissements, 
     solde                        AS flux_net_mensuel, 
  
-    -- Correction : Reconstruction de la position cumulée par exercice
+    -- Reconstruction de la position cumulée par exercice
     SUM(solde) OVER (
         PARTITION BY code_societe, compte_gl, annee
         ORDER BY annee_mois
@@ -37,4 +37,5 @@ SELECT
     )                            AS flux_moyen_3_mois 
  
 FROM {{ ref('gd_balance_comptable') }} 
-WHERE SUBSTR(CAST(compte_gl AS STRING), 1, 3) IN ('512', '514', '516', '530')
+-- Version correcte pour BigQuery
+WHERE REGEXP_CONTAINS(CAST(compte_gl AS STRING), r'^(401|601)')
